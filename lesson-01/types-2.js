6452f09e9a7381abc22f09aba9b2be50ed13ae5b
Adam Pocs <adam.pocs@odigeo.com>
2017-06-21 20:32:25 +0200

    Add Types 2 (Arrays) to Lesson 01.

---
+++ lesson-01/types-2.js
@@ -0,0 +1,20 @@
+// Arrays
+
+var myArray = [
+    45,
+    87,
+    true,
+    false,
+    true,
+    "some string here"
+];
+
+var thisWasTheLastOne = myArray.pop();
+console.log(thisWasTheLastOne);
+console.log(myArray);
+thisWasTheLastOne = myArray.pop();
+console.log(thisWasTheLastOne);
+console.log(myArray);
+thisWasTheLastOne = myArray.pop();
+console.log(thisWasTheLastOne);
+console.log(myArray);
